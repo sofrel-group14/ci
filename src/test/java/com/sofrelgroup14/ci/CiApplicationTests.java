eed7a3cbebb959942cbbc3d412dddd14b6680eb1
Taqui <taqui@kth.se>
2021-02-05 19:51:06 +0100

    (feat) Created Base Files for Spring Boot Project

---
+++ src/test/java/com/sofrelgroup14/ci/CiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofrelgroup14.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
