0298749a0a87a3e6d6cf11b37406944254c79133
Taqui <taqui@kth.se>
2021-02-05 19:51:06 +0100

    (feat) Created Base Files for Spring Boot Project
    related to #9

---
+++ src/main/java/com/sofrelgroup14/ci/CiApplication.java
@@ -0,0 +1,13 @@
+package com.sofrelgroup14.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiApplication.class, args);
+	}
+
+}
