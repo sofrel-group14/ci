9c331d7a2ddbb6c6d52b64af7cb9b002be03ea97
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-02-10 18:27:36 +0100

    Failure

--- src/test/java/com/sofrelgroup14/ci/CiApplicationTests.java
+++ src/test/java/com/sofrelgroup14/ci/CiApplicationTests.java
@@ -3,11 +3,14 @@ package com.sofrelgroup14.ci;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class CiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(true);
 	}
 
 }
