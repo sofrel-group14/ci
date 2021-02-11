ca216aa547dbc564a9c388ed2fa048a302ae829a
Taqui <taqui@kth.se>
2021-02-11 12:15:40 +0100

    (doc) Improved JavaDoc in LogsController, Logs, LogsRepository
    Fixes: #41

--- src/main/java/com/sofrelgroup14/ci/controller/LogsController.java
+++ src/main/java/com/sofrelgroup14/ci/controller/LogsController.java
@@ -14,17 +14,34 @@ import com.sofrelgroup14.ci.repositories.LogsRepository;
 import org.bson.types.ObjectId;
 import java.util.List;
 
+
+
+/**
+ * Rest controller for the /logs-endpoint, invoked by 
+ * the frontend to check the build logs
+ * The endpoints either return all logs or a log with a specific ID.
+ */
+
 @RestController
 @RequestMapping("/logs")
 public class LogsController {
     @Autowired
     private LogsRepository repository;
 
+    /**
+     * The logs-endpoints return a all build logs with in the database
+     * @return List<Logs>
+     */
+
     @RequestMapping(value = "", method = RequestMethod.GET)
     public List<Logs> getAllLogs() {
         return repository.findAll();
     }
-
+    /**
+     * The logs/{id}-endpoint return a specific log with the id which is sent as a URL paramenter
+     * @param id unique id for identifying each log 
+     * @return Logs
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Logs getLogById(@PathVariable("id") ObjectId id) {
         return repository.findBy_id(id);
